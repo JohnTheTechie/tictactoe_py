b5a685427f23ab9de7765753e133d45da74df0a5
JohnTheTechie <janakiraman3394@gmail.com>
2020-11-01 22:18:44 +0530

    communication queues created

---
+++ tcp/network_config.py
@@ -0,0 +1,18 @@
+
+import socket
+
+
+class NetworkConfiguration:
+
+    __singleton = None
+
+    def __init__(self):
+        self.local_machine_id = socket.gethostbyname(socket.gethostname())
+        self.network_id = self.local_machine_id.rsplit(".")[0]
+        self.configured_application_port = 10101
+        self.maximum_allowed_connection = 5
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__singleton is None:
+            cls.__singleton = object.__new__(cls)
+        return cls.__singleton
